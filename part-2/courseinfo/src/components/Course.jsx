0bf3080a19232140b9f56bfe6154ad67147344c3
Niharika Hari <hariniharika12@gmail.com>
2024-10-09 17:41:10 +0530

    Add files for part2 courseinfo
    Exercises 2.1 to 2.5

---
+++ part-2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,38 @@
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
+
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) => (
+        <Part key={part.id} part={part.name} exercises={part.exercises}></Part>
+      ))}
+    </>
+  );
+};
+
+const Total = ({ parts }) => {
+  let total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <b>total of {total} exercises</b>;
+};
+
+export default Course;
